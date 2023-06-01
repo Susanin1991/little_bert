c10dfadaedc5fb1cb07138eea2c29e43d3530c81
Susanin <65289165+Susanin1991@users.noreply.github.com>
2023-06-01 09:28:46 +0300

    Finished simple flask client-server

--- flask_client.py
+++ flask_client.py
@@ -1,17 +1,25 @@
 import requests
 
-# Set the server URL
-server_url = 'http://169.255.254.2:5000/process'
 
-def send_data(data):
+def send_data(message):
     # Send a POST request to the server
-    response = requests.post(server_url, json=data)
+    response = requests.post(url, json=message)
     if response.status_code == 200:
         result = response.json()
-        print(result['message'])
+        print(result['response'])
     else:
         print('Error:', response.status_code)
 
-# Example usage
-data = {'name': 'John', 'age': 30}
-send_data(data)
+
+def client_program():
+    message = ""  # take input
+    while message.lower().strip() != 'bye':
+
+        message = input(" -> ")
+        data = {'message': message}
+        send_data(data)
+
+
+if __name__ == '__main__':
+    url = 'http://localhost:5000/process'
+    client_program()
--- flask_server.py
+++ flask_server.py
@@ -1,22 +1,23 @@
 import os
 import sys
 import socket
-
 import transformers
-from flask import Flask, request
 import numpy
 import torch
 import pickle
 
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/process', methods=['POST'])
 def process_data():
-    data = request.get_json()
-    # Process the data as needed
-    result = {'message': 'Data processed successfully'}
-    return result
+    message = request.get_json()
+    message = bert_process(message)
+    response = {'response': 'Bert result is: ' + message}
+    return jsonify(response)
+
 
 def process_text(text):
     tokenized = tokenizer.encode(text, add_special_tokens=True)
@@ -26,6 +27,21 @@ def process_text(text):
     with torch.no_grad():
         last_hidden_states = model(input_ids)[0]
     return last_hidden_states[:, 0, :].numpy()
+
+
+def bert_process(text):
+    print('message is: ', text)
+    input_features = process_text(text['message'])
+    result = lr_clf.predict(input_features)
+    data = ''
+    if result == [0]:
+        data = "BERT thinks it's bad thing to say"
+        print(data, result)
+    if result == [1]:
+        data = "BERT thinks it's good thing to say"
+        print(data, result)
+
+    return data
 
 
 if __name__ == '__main__':
@@ -47,6 +63,7 @@ if __name__ == '__main__':
         print('model is missing')
         sys.exit()
 
-    host = socket.gethostname()
-    port = 5000
-    app.run(host=host, port=port)
+    # host = "127.0.0.1"
+    # port = 8000
+    # app.run(host=host, port=port)
+    app.run()
